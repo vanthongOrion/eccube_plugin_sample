122162a5d7cd55ae899948f4ee08d613ab81cceb
root <root@localhost.localdomain>
2024-04-09 16:02:58 +0900

    create sample plugin

---
+++ app/Plugin/sample/Nav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\sample;
+
+use Eccube\Common\EccubeNav;
+
+class Nav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [];
+    }
+}
